01e8d9eb2d80ed88cadbf7d9180bb2518af12d74
Mogulsam <theejaguarscomedy@gmail.com>
2023-12-13 01:37:41 -0800

    Shell project updated11

--- tokenizer.c
+++ tokenizer.c
@@ -1,24 +1,23 @@
 #include "shell.h"
 
 /**
- * **strtow - splits the  string into the words. Repeat delimiters are ignored
- * @str: the input strings vahhv
- * @d: the delimeter stringsdf
- gfhfyyhvvvnv
- @fhkhkhknkhjhkui
+ * **strtow - splits a string into words. Repeat delimiters are ignored
+ * @str: the input string
+ * @d: the delimeter string
  * Return: a pointer to an array of strings, or NULL on failure
  */
+
 char **strtow(char *str, char *d)
 {
-	int p, j, k, m, numwords = 0;
+	int i, j, k, m, numwords = 0;
 	char **s;
 
 	if (str == NULL || str[0] == 0)
 		return (NULL);
 	if (!d)
 		d = " ";
-	for (p = 0; str[p] != '\0'; p++)
-		if (!is_delim(str[p], d) && (is_delim(str[p + 1], d) || !str[p + 1]))
+	for (i = 0; str[i] != '\0'; i++)
+		if (!is_delim(str[i], d) && (is_delim(str[i + 1], d) || !str[i + 1]))
 			numwords++;
 
 	if (numwords == 0)
@@ -26,12 +25,12 @@ char **strtow(char *str, char *d)
 	s = malloc((1 + numwords) * sizeof(char *));
 	if (!s)
 		return (NULL);
-	for (p = 0, j = 0; j < numwords; j++)
+	for (i = 0, j = 0; j < numwords; j++)
 	{
-		while (is_delim(str[j], d))
-			p++;
+		while (is_delim(str[i], d))
+			i++;
 		k = 0;
-		while (!is_delim(str[p + k], d) && str[p + k])
+		while (!is_delim(str[i + k], d) && str[i + k])
 			k++;
 		s[j] = malloc((k + 1) * sizeof(char));
 		if (!s[j])
@@ -52,32 +51,31 @@ char **strtow(char *str, char *d)
 /**
  * **strtow2 - splits a string into words
  * @str: the input string
- * @d: the delimeter diametetr  hjfhgvkj
- hhgvhjgjjv
- * Return: a pointer uhjkhjgb to an array of strings, or NULL on failure
+ * @d: the delimeter
+ * Return: a pointer to an array of strings, or NULL on failure
  */
 char **strtow2(char *str, char d)
 {
-	int p, j, k, m, numwords = 0;
+	int i, j, k, m, numwords = 0;
 	char **s;
 
 	if (str == NULL || str[0] == 0)
 		return (NULL);
-	for (p = 0; str[p] != '\0'; p++)
-		if ((str[p] != d && str[p + 1] == d) ||
-		    (str[p] != d && !str[p + 1]) || str[p + 1] == d)
+	for (i = 0; str[i] != '\0'; i++)
+		if ((str[i] != d && str[i + 1] == d) ||
+		    (str[i] != d && !str[i + 1]) || str[i + 1] == d)
 			numwords++;
 	if (numwords == 0)
 		return (NULL);
 	s = malloc((1 + numwords) * sizeof(char *));
 	if (!s)
 		return (NULL);
-	for (p = 0, j = 0; j < numwords; j++)
+	for (i = 0, j = 0; j < numwords; j++)
 	{
-		while (str[p] == d && str[p] != d)
-			j++;
+		while (str[i] == d && str[i] != d)
+			i++;
 		k = 0;
-		while (str[p + k] != d && str[p + k] && str[p + k] != d)
+		while (str[i + k] != d && str[i + k] && str[i + k] != d)
 			k++;
 		s[j] = malloc((k + 1) * sizeof(char));
 		if (!s[j])
@@ -88,7 +86,7 @@ char **strtow2(char *str, char d)
 			return (NULL);
 		}
 		for (m = 0; m < k; m++)
-			s[j][m] = str[p++];
+			s[j][m] = str[i++];
 		s[j][m] = 0;
 	}
 	s[j] = NULL;
